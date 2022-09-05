4ac62384b8b9b00c6835bd2970d1971d80a8b836
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-09-05 22:11:06 +0300

    Update index.py

--- 1 - Introduction/index.py
+++ 1 - Introduction/index.py
@@ -1,1 +1,7 @@
-print("Hi")
+# name = input("Enter your name ")
+# print("Hello " + name)
+
+first_number = int(input("Enter a number "))
+second_number = int(input("Enter a number "))
+print("sum")
+print(first_number + second_number)
