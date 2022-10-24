e10e3f80a95e5af8eb5faf88682ae1fc312fa29f
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-10-24 19:06:39 +0300

    Create functions.py

---
+++ 3 - Control Flow and Funtions/functions.py
@@ -0,0 +1,21 @@
+# Function
+# def dog():
+#     # code here gets executed when you call the function
+#     print("Arf Arf")
+
+
+# dog()
+
+# Arguments/Paremeter
+
+
+def dog(mood):
+    # code here gets executed when you call the function
+    if mood == "happy":
+        print("Arf Arf")
+    else:
+        print(mood)
+
+
+dog("happy")
+dog("sad")
